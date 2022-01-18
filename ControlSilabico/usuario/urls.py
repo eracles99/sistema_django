aec2831e3e659d75dc438cc4a91ce7ee467919d8
Nelson <160923@unsaac.edu.pe>
2022-01-17 20:52:29 -0500

    17 de enero 8:52

--- ControlSilabico/usuario/urls.py
+++ ControlSilabico/usuario/urls.py
@@ -4,6 +4,7 @@ from ControlSilabico.usuario.views  import *
 from django.conf.urls.static import static
 from django.conf  import settings
 urlpatterns = [
-    path('crear_usuario/',asignation, name='crear_usuario'),
+    #path('crear_usuario/',asignation, name='crear_usuario'),
+    path('crear_usuario_docente/',registrar_usuario_docente, name='crear_usuario_docente'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
